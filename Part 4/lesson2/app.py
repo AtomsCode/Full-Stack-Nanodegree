c0cda9fefe30a919c785acf61bc188200b058c5a
AtomCode <mr.ali.alhelal@gmail.com>
2020-05-29 11:48:21 +0300

    define get_token_header function

--- Part 4/lesson2/app.py
+++ Part 4/lesson2/app.py
@@ -1,12 +1,26 @@
-from flask import Flask , request
+from flask import Flask , request , abort
 
 app = Flask(__name__)
 
+def get_token_header():
+
+    if 'Authorization' not in request.headers:
+        abort(401)
+
+    auth_header = request.headers['Authorization']
+    header_parts = auth_header.split(' ')
+
+    if len(header_parts) != 2:
+        abort(401)
+    elif header_parts[0].lower().strip() != 'bearer':
+        abort(401)
+    return header_parts[1]
 
 @app.route('/headers')
 def headers():
 
-    auth_header = request.headers['Authorization']
+    jwt = get_token_header()
 
-    print(auth_header)
+    print(jwt)
+
     return "not implimented"
